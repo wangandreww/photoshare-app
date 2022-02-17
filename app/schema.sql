dca5ae7b28fa180c0d253474d9a971c89b52b42f
Huy Phan <phan.q.huy21@gmail.com>
2022-02-17 15:09:16 -0500

    Added photo tags album tags albums comments

--- app/schema.sql
+++ app/schema.sql
@@ -4,9 +4,9 @@ DROP TABLE IF EXISTS Pictures CASCADE;
 DROP TABLE IF EXISTS Users CASCADE;
 
 CREATE TABLE Users (
-    user_id int4  AUTO_INCREMENT,
+    user_id int4 AUTO_INCREMENT,
     email varchar(255) UNIQUE,
-    password varchar(255),
+    user_password varchar(255),
     first_name varchar(255),
     last_name varchar(255),
     DOB DATE
@@ -16,16 +16,14 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE Friends (
-    pair_id int4  AUTO_INCREMENT,
+    pair_id int4 AUTO_INCREMENT,
     user_id 
     friend_id
 );
 
-
-
 CREATE TABLE Pictures
 (
-  picture_id int4  AUTO_INCREMENT,
+  picture_id int4 AUTO_INCREMENT,
   user_id int4,
   imgdata longblob ,
   caption VARCHAR(255),
@@ -35,3 +33,32 @@ CREATE TABLE Pictures
 INSERT INTO Users (email, password) VALUES ('test@bu.edu', 'test');
 INSERT INTO Users (email, password) VALUES ('test1@bu.edu', 'test');
 
+CREATE TABLE PictureTags
+(
+  picture_id,
+  tag_description VARCHAR(255),
+  FOREIGN KEY picture_id REFERENCES Pictures(picture_id)
+);
+
+CREATE TABLE Album
+(
+  album_id int4 AUTO_INCREMENT,
+  album_name VARCHAR(255),
+  user_id VARCHAR(255),
+  create_date VARCHAR(255),
+  CONSTRAINT album_pk PRIMARY KEY (album_id) 
+);
+
+CREATE TABLE AlbumTags(
+  album_id,
+  tag_description VARCHAR(255),
+  FOREIGN KEY album_id REFERENCES Album(album_id)
+);
+
+CREATE TABLE Comments(
+  comment_id,
+  comment_description VARCHAR(255),
+  comment_timestamp TIME,
+  user_id VARCHAR(255),
+  CONSTRAINT comment_pk PRIMARY KEY (comment_id)
+);
